9b3abe08888b81f494a5386617d6e802e5950312
Dhruv Mohindru <dmohindru@gmail.com>
2023-08-08 07:26:21 +1000

    making progress

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -13,4 +13,41 @@ test('it shows two inputs and a button', () => {
     // Assertion - make sure the component is doing what we expect it to do
     expect(inputs).toHaveLength(2);
     expect(button).toBeInTheDocument();
-})
+});
+
+test('it calls onUserAdd when the form is submitted', () => {
+    // Mock function
+    const mock = jest.fn();
+
+    // Try to render my component
+    render(<UserForm onUserAdd={mock} />);
+
+    // Fin the two inputs
+    const nameInput = screen.getByRole('textbox', {
+        name: /name/i
+    });
+
+    const emailInput = screen.getByRole('textbox', {
+        name: /email/i
+    });
+
+
+
+    // Simulate type in a name
+    user.click(nameInput);
+    user.keyboard('jane');
+
+    // Simulate typing in an email
+    user.click(emailInput);
+    user.keyboard('jane@jane.com');
+
+    // Find the button
+    const button = screen.getByRole('button');
+
+    // Simulate clicking the button
+    user.click(button);
+
+    // Assertion to make sure 'onUserAdd' get called with email/name
+    expect(mock).toHaveBeenCalled();
+    expect(mock).toHaveBeenCalledWith({ name: 'jane', email: 'jane@jane.com'});
+});
