9b4e63dfaf83d588f87a90a313d902d7e2e922cd
Dhruv Mohindru <dmohindru@gmail.com>
2023-08-10 07:13:36 +1000

    Making progress through this section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import { useState } from "react";
-import UserForm from "./UserForm";
+import { useState } from 'react';
+import UserForm from './UserForm';
 import UserList from './UserList';
 
 function App() {
   const [users, setUsers] = useState([]);
+
   const onUserAdd = (user) => {
     setUsers([...users, user]);
-  }
+  };
+
   return (
     <div>
       <UserForm onUserAdd={onUserAdd} />
